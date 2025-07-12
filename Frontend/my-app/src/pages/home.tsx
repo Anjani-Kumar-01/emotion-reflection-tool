7285402bba44161d6ce8202743cba950d76116a1
Anjani Kumar <anjanikumar70157@gmail.com>
2025-07-12 11:38:05 +0530

    commited

--- Frontend/my-app/src/pages/home.tsx
+++ Frontend/my-app/src/pages/home.tsx
@@ -44,7 +44,7 @@ export default function Home() {
         />
         <button
           type="submit"
-          className="bg-blue-600  hover:bg-blue-700  text-white px-4 py-2  rounded alignitems-center justify-center"
+          className="bg-blue-600  hover:bg-blue-700  text-white px-4 py-2  rounded alignitems-center justify-cent"
         >
           Submit
         </button>
